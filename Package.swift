daa4fe79af99170727009729b7299e13b3046791
Niclas Flysjö <niclas@flysjo.com>
2024-03-28 16:04:52 +0100

    Updated minimum version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Leanplum",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "Leanplum", targets: ["LeanplumTargetWrapper"]),
